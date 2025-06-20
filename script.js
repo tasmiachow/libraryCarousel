d3428ee385f0015ce5bccc8c61e29ef676084e84
Tasmia Chowdhury <tasmiachowdhury123@gmail.com>
2025-06-20 12:34:39 -0400

    working carousel js

--- script.js
+++ script.js
@@ -27,15 +27,17 @@ function addBookToLibrary(book){
     myLibrary.push(book);
     const card = document.createElement("div");
     card.classList.add("card");
+    card.id = myLibrary.length -1;
     card.textContent = book.info();
     container.appendChild(card);
-
+    updateDisplay()
 
 };
 
 function backDisplay(e){
     if (currentCard>0){
          currentCard -= 1;
+         updateDisplay()
     }
     else{
         alert("Hey you're back at the beginning of the library. Press foward");
@@ -45,6 +47,7 @@ function backDisplay(e){
 function forwardDisplay(e){
     if(currentCard < myLibrary.length -1){
         currentCard+=1;
+        updateDisplay()
     }
     else{
         alert("Hey this is the end of the library try adding more books and refreshing!");
@@ -62,10 +65,18 @@ forwardButton.addEventListener("click", ()=>{
 
 //hide other cards only show currentCard
 function updateDisplay(e){
-
+    for(let i=0; i<myLibrary.length; i++){
+        document.getElementById(i.toString()).classList.add('hidden');
+    }
+    document.getElementById(currentCard.toString()).classList.remove('hidden');
 };
 
 const book1 = new Book("The Hobbit", "J.R.R. Tolkien", 295, false);
+const book2 = new Book("Harry Potter", "J.K rowling", 395, true);
+const book3 = new Book("Pheonix King", "Aparma Verma", 500, true);
 console.log(book1.info());
 
 addBookToLibrary(book1);
+addBookToLibrary(book2);
+addBookToLibrary(book3);
+
